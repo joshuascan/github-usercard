40498f6fb832594215846237f75e1549d5bdec7d
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-05-06 14:23:30 -0700

    Completed step 4

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -6,8 +6,16 @@ import axios from "axios";
     https://api.github.com/users/<your name>
 */
 
-axios.get(`https://api.github.com/users/joshuascan`);
-
+axios.get(`https://api.github.com/users/joshuascan`)
+    .then((response) => {
+        const data = response.data;
+        // data.forEach( data => {
+            // const profileCard = githubCardMaker(data);
+            cards.appendChild(githubCardMaker(data));
+    })
+    .catch((err) => {
+        console.log('Error: ', err);
+    })
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -20,7 +28,7 @@ axios.get(`https://api.github.com/users/joshuascan`);
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
-
+    
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
     follow this link in your browser https://api.github.com/users/<Your github name>/followers,
@@ -64,22 +72,13 @@ function githubCardMaker(obj) {
     const username = document.createElement('p');
     const location = document.createElement('p');
     const profile = document.createElement('p');
-    const profileLink = document.createElement('a');
+    const address = document.createElement('a');
     const followers = document.createElement('p');
     const following = document.createElement('p');
     const userBio = document.createElement('p');
 
-    card.appendChild(userImage);
-    card.appendChild(cardInfo);
-    cardInfo.appendChild(name);
-    cardInfo.appendChild(username);
-    cardInfo.appendChild(location);
-    cardInfo.appendChild(profile);
-    profile.appendChild(profileLink);
-    cardInfo.appendChild(followers);
-    cardInfo.appendChild(following);
-    cardInfo.appendChild(userBio);
 
+    
     card.classList.add('card');
     cardInfo.classList.add('card-info');
     name.classList.add('name');
@@ -89,12 +88,25 @@ function githubCardMaker(obj) {
     name.textContent = obj.name;
     username.textContent = obj.login;
     location.textContent = `Location: ${obj.location}`;
-    profile.textContent = 'Profile:';
-    profileLink.href = obj.html_url;
-    profileLink.textContent = obj.html_url;
+    profile.textContent = 'Profile: ';
+    address.href = obj.html_url;
+    address.textContent = obj.html_url;
     followers.textContent = `Followers: ${obj.followers}`;
     following.textContent = `Following: ${obj.following}`;
     userBio.textContent = `Bio: ${obj.bio}`;
+
+    card.appendChild(userImage);
+    card.appendChild(cardInfo);
+    cardInfo.appendChild(name);
+    cardInfo.appendChild(username);
+    cardInfo.appendChild(location);
+    cardInfo.appendChild(profile);
+    profile.appendChild(address);
+    cardInfo.appendChild(followers);
+    cardInfo.appendChild(following);
+    cardInfo.appendChild(userBio);
+
+    return card;
 }
 
 /*
